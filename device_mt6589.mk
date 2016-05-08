b63caf0c37fb5a2abe409efcbeb138e8359876c2
Joel Bueno Calvache <buenocalvachejoel@gmail.com>
2016-05-08 19:51:53 +0200

    sepolicy: untrusted_app.te

--- device_mt6589.mk
+++ device_mt6589.mk
@@ -61,6 +61,7 @@ BOARD_SEPOLICY_UNION := \
        surfaceflinger.te \
        pvrsrvctl.te \
        kernel.te \
+       untrusted_app.te \
        file_contexts
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
